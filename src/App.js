8d3efb28254315a938cf5c9c776031b87385637e
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2025-11-27 19:24:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css'
 import React from 'react'
-import search from './search'
+import search1 from './search'
 
 function App() {
   const handleClick = () => {
 
-    const randSearch = search[Math.floor(Math.random() * (1266 - 1 + 1) + 1 )]
+    const search2 = JSON.parse(localStorage.getItem('invidious'))
+    if(!search2){
+      localStorage.setItem('invidious', JSON.stringify(search1))
+      return
+    }
+    const randSearch = search2[Math.floor(Math.random() * (search2.length - 1 + 1) + 1 )]
+
+    const newInvidious = search2.filter(item => item !== randSearch)
+    localStorage.setItem('invidious', JSON.stringify(newInvidious))
     
     window.location = `https://inv.nadeko.net/search?q=${randSearch}`
     
